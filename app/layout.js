61deaa995024ea40eb3f069431b58f7c695784c0
Vr4phxtz <namzakiller01@gmail.com>
2025-06-30 08:23:28 +0700

    updatev.1

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navigation from "./components/Navigation";
